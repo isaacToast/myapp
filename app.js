2db33adf2ecc0ef5a5fd29ce008fc96b1ae4c0c0
isaacToast <qocnfgkqtlek@naver.com>
2016-04-20 19:03:29 +0900

    board end

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
 
 // connect database
 mongoose.connect(process.env.mongo_DB);
@@ -30,6 +31,8 @@ app.set("view engine", 'ejs');
 // set middlewares
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ectended:true}));
+app.use(methodOverride("_method"));
 
 // set routes
 app.get('/posts', function(req,res){
@@ -38,10 +41,14 @@ app.get('/posts', function(req,res){
 		res.render("posts/index", {data:posts});
 	});
 }); // index
+app.get('/posts/new', function(req,res){
+	res.render("posts/new");
+}); // new
 app.post('/posts', function(req,res){
+	console.log(req.body);
 	Post.create(req.body.post,function (err, post){
 		if(err) return res.json({success:false, mesage:err});
-		res.json({success:true, data:post});
+		res.redirect('/posts');
 	});
 }); // create
 app.get('/posts/:id', function(req,res){
@@ -50,17 +57,23 @@ app.get('/posts/:id', function(req,res){
 		res.render("posts/show", {data:post});
 	});
 }); // show
+app.get('/posts/:id/edit', function(req,res){
+	Post.findById(req.params.id, function (err,post){
+		if(err) return res.json({success:false, message:err});
+		res.render("posts/edit", {data:post});
+	});
+}); // edit
 app.put('/posts/:id', function(req,res){
 	req.body.post.updatedAt=Date.now();
 	Post.findByIdAndUpdate(req.params.id, req.body.post, function (err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, message:post._id+" updated"});
+		res.redirect('/posts/'+req.params.id);
 	});
 }); // update
 app.delete('/posts/:id', function(req,res){
 	Post.findByIdAndRemove(req.params.id, function (err,post){
 		if(err) return res.json({success:false, message:err});
-		res.json({success:true, message:post._id+" deleted"});
+		res.redirect('/posts');
 	});
 }); // destroy
 
